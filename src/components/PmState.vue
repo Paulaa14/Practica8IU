65f99e1b012297d67d84e775b6f3747b612acf23
juanpf04 <juanpf04@ucm.es>
2024-11-25 19:24:07 +0100

    prueba

--- src/components/PmState.vue
+++ src/components/PmState.vue
@@ -3,7 +3,7 @@
   <!-- Navbar principal -->
   <nav class="navbar navbar-expand-lg">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">PlanDoc</a>
+      <a class="navbar-brand" href="#">JP</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
