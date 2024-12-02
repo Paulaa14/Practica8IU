6b89a68706c9ffe7018f65f9e58844d5e9e3523e
Manuel Freire <manuel.freire@fdi.ucm.es>
2024-12-02 15:28:12 +0100

    Added missing components for demo

---
+++ src/components/ConfirmModal.vue
@@ -0,0 +1,39 @@
+<template>
+    <BaseModal ref="modalRef" id="confirmModal"
+      :title="'Confirma acción peligrosa'">
+      <template #body>
+        <span>{{ text }}</span>
+      </template>
+      <template #footer>
+        <button @click.prevent="() => action()" class="btn btn-primary">
+          {{ 'Confirmar acción' }}
+        </button>
+      </template>
+    </BaseModal>
+</template>
+
+<script setup>
+
+import BaseModal from './BaseModal.vue';
+
+import { ref } from 'vue'
+
+const props = defineProps({
+  text: String,
+  action: Function
+})
+
+let modalRef = ref(null);
+
+// para que el padre pueda llamar a show (hide no debería hacer falta)
+function show() {
+  modalRef.value.show();
+}
+defineExpose({ show });
+</script>
+
+<style scoped>
+    .name {
+        font-weight: 200%;
+    }
+</style>
---
+++ src/components/DropdownBox.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="row g-1 form-inline align-items-baseline">
+        <div class="col-3 text-end">
+            <label :for="id" class="form-label">{{ label }}</label>
+        </div>
+        <div class="col-auto">
+            <select class="form-select" :name="id" :id="id" @input="$emit('change', value);">
+                <option value="FALL">1er Cuatrimestre</option>
+                <option value="SPRING">2º Cuatrimestre</option>
+            </select>
+            <span>{{value}}</span>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const props = defineProps({
+    label: String,
+    id: String,
+    start: String,
+})
+defineEmits(['change'])
+
+let value = ref(0)
+
+onMounted(() => {
+    value.value = props.start;
+})
+
+</script>
+
+<style scoped>
+* {
+    background-color: lightblue;
+}
+</style>
